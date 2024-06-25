44b669b8b9a073de3e4ff96b172fc775a2b8f748
Sequentz <wouterkvandenbossche@gmail.com>
2024-06-25 16:02:43 +0200

    fix add category

--- resources/views/addproduct.blade.php
+++ resources/views/addproduct.blade.php
@@ -29,9 +29,7 @@
                             </option>
                             @endforeach
                         </select>
-                        <button type="button" id="openModalButton" class="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            Add Brand
-                        </button>
+
                     </div>
                     @error('brand_id')
                     <p class="text-red-600 text-lg mt-1">{{ $message }}</p>
@@ -49,9 +47,7 @@
                             </option>
                             @endforeach
                         </select>
-                        <button type="button" id="openModalButtonCategory" class="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            Add Category
-                        </button>
+
                     </div>
                     @error('category_id')
                     <p class="text-red-600 text-lg mt-1">{{ $message }}</p>
@@ -91,47 +87,6 @@
         </form>
     </div>
 
-    <!-- Main modal -->
-    <div id="category-modal" class="hidden fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
-        <div class="relative mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
-            <!-- Modal header -->
-            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
-                <h3 class="text-lg font-semibold text-gray-900">
-                    Create New Category
-                </h3>
-                <button type="button" id="closeModalButton" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center">
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                    </svg>
-                    <span class="sr-only">Close modal</span>
-                </button>
-            </div>
-            <!-- Modal body -->
-            <form id="category-form" class="p-4 md:p-5" method="POST" action="{{ route('categories.store') }}" enctype="multipart/form-data">
-                @csrf
-                <div class="grid gap-4 mb-4 grid-cols-1">
-                    <div>
-                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Category Name</label>
-                        <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Type category name" required>
-                    </div>
-                    <div>
-                        <label for="description" class="block mb-2 text-sm font-medium text-gray-900">Category Description</label>
-                        <input type="text" name="description" id="description" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Type category description" required>
-                    </div>
-                    <div>
-                        <label for="img" class="block mb-2 text-sm font-medium text-gray-900">Category Image</label>
-                        <input type="file" name="img" id="img" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" accept="image/*">
-                    </div>
-                </div>
-                <button type="submit" class="w-full text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-                    <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
-                    </svg>
-                    Add Category
-                </button>
-            </form>
-        </div>
-    </div>
 
     <script>
         document.getElementById('openModalButton').addEventListener('click', function() {
