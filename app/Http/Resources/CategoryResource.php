0ee4342fb42824a3d1e5daf6af1f6c3d254c016b
Sequentz <wouterkvandenbossche@gmail.com>
2024-06-25 19:50:39 +0200

    fix categories response

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -16,7 +16,7 @@ class CategoryResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'category' => $this->category,
+            'category' => $this->name,
             'description' => $this->description,
             'image' => $this->image
         ];
